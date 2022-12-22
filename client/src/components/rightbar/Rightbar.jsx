1ee26094c8f739fb69a279028bda15814e69192a
DominikJ22 <dominikjo22@gmail.com>
2022-12-21 21:40:23 -0500

    back and front end

---
+++ client/src/components/rightbar/Rightbar.jsx
@@ -0,0 +1,105 @@
+import "./rightbar.css";
+import { Users } from "../../Data";
+import Online from "../online/Online";
+
+export default function Rightbar({ profile }) {
+  const HomeRightbar = () => {
+    return (
+      <>
+        <div className="weightContainer">
+          <img className="weightImg" src="assets/weight.png" alt="" />
+          <span className="weightText">
+            <b>Bob Boberson</b> and <b>3 other friends</b> have worked out today!
+          </span>
+        </div>
+        <img className="rightbarAd" src="assets/ad.png" alt="" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendList">
+          {Users.map((u) => (
+            <Online key={u.id} user={u} />
+          ))}
+        </ul>
+      </>
+    );
+  };
+
+  const ProfileRightbar = () => {
+    return (
+      <>
+        <h4 className="rightbarTitle">User information</h4>
+        <div className="rightbarInfo">
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">City:</span>
+            <span className="rightbarInfoValue">New York</span>
+          </div>
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">From:</span>
+            <span className="rightbarInfoValue">USA</span>
+          </div>
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">Fitness Level:</span>
+            <span className="rightbarInfoValue">Level 3</span>
+          </div>
+        </div>
+        <h4 className="rightbarTitle">User friends</h4>
+        <div className="rightbarFollowings">
+          <div className="rightbarFollowing">
+            <img
+              src="assets/person/1.jpg"
+              alt=""
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">Bob Boberson</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              src="assets/person/2.jpg"
+              alt=""
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">Sarah Jackson</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              src="assets/person/3.jpg"
+              alt=""
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">Amber Dono</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              src="assets/person/4.jpg"
+              alt=""
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">Maya Jackson</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              src="assets/person/5.jpg"
+              alt=""
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">Jackson Down</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              src="assets/person/6.jpg"
+              alt=""
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">Tri Bucky</span>
+          </div>
+        </div>
+      </>
+    );
+  };
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        {profile ? <ProfileRightbar /> : <HomeRightbar />}
+      </div>
+    </div>
+  );
+}

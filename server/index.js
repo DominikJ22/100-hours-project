5d91d7d3fce9b455456344142a68df9e6bbcca18
DominikJ22 <dominikjo22@gmail.com>
2023-01-02 22:42:48 -0500

    auth and login and password

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js"
 import { register } from "./controllers/auth.js"
 
 
@@ -43,6 +44,8 @@ const upload = multer({ storage });
 /*ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register)
 
+/*ROUTES */
+SVGClipPathElement.post("/auth/register", upload.single("picture"),register)
 
 /*Mongoose SET UP */
 const PORT = process.env.PORT || 3001;
